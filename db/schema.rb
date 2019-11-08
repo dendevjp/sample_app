8644687036c1f173f0e5525a19aa257cca03a65d
dendevjp <dendevjp@gmail.com>
2019-11-09 02:14:00 +0900

    20191109

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_143315) do
+ActiveRecord::Schema.define(version: 2019_11_08_134324) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 2019_11_07_143315) do
     t.datetime "created_at", null: false
     t.string "remember_digest"
     t.boolean "admin"
+    t.string "activation_digest"
+    t.boolean "activated"
+    t.datetime "activated_at"
   end
 
 end
